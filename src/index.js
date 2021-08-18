f3208bafb845995c5f62416088ac286e3e63ad09
Jinui You <baram991103@gmail.com>
2021-08-18 12:43:44 +0900

    :sparkles: Make gathering domain

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// 모든 dom event 리슨
+// https://stackoverflow.com/questions/27321672/listen-for-all-events-in-javascript/27322253
+
+
+// SDK 실행시 도메인 등록
+// window.onload 함수 사용
+window.onload = () => {
+  const domain = window.location.host;
+  // TODO API 연결
+};
+
+
+// http reqeust 가로채기
+// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Intercept_HTTP_requests
+
+
+
+// TODO DB 스키마
+/*
+- 위협명 (thunder_name)
+- 위협 판별의 근거가 된 데이터, statusCode나 insecureCode (thunder_source)
+- 제안 (suggestion)
+- 참고 사이트 (ref_link)
+- 우선순위 (priority)
+- 코멘트 (comment)
+*/

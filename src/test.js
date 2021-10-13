cf11e4c1cc9e6adfbeaa5214d4735188b70d6cc1
Jinui You <baram991103@gmail.com>
2021-10-13 20:46:51 +0900

    :heavy_plus_sign: Add jest

---
+++ src/test.js
@@ -0,0 +1,32 @@
+const Xss = require('./weakness/xss');
+const SqlInjection = require('./weakness/sqlinjection');
+
+describe('xss test', () => {
+  it('detect case', (done) => {
+    const isDetected = Xss.checkString('<xss id=x tabindex=1 onactivate=alert(1)></xss>');
+    expect(isDetected).toBeTruthy();
+    done();
+  });
+
+  it('undetect case', (done) => {
+    const isDetected = Xss.checkString('Hi, It is xss');
+    expect(isDetected).not.toBeTruthy();
+    done();
+  });
+});
+
+
+describe('sqlinjection test', () => {
+  it('detect case', (done) => {
+    const isDetected = SqlInjection.checkString('select * from 1');
+    expect(isDetected).toBeTruthy();
+    done();
+  });
+
+  it('undetect case', (done) => {
+    const isDetected = SqlInjection.checkString('Hi, It is sqlinjection');
+    expect(isDetected).not.toBeTruthy();
+    done();
+  });
+});
+

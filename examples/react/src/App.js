7dc27fec4009cd5d9e3c57fab7bddc26993d1d32
Jinui You <baram991103@gmail.com>
2021-10-16 18:22:54 +0900

    :lipstick: Add test form

--- examples/react/src/App.js
+++ examples/react/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import logo from './logo.svg';
+import { useState } from 'react';
 import { protect } from 'cumulus';
 
 protect({
@@ -7,11 +8,39 @@ protect({
 });
 
 function App() {
+  const [keyword, setKeyword] = useState('');
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <input style={{ height: 80, width: '50vh', borderRadius: 20, fontSize: 24, paddingLeft: 24, }} placeholder="검색어를 입력하세요"/>
+
+        <h3>Text form</h3>
+        <input
+          style={{ height: 80, width: '50vh', borderRadius: 20, fontSize: 24, paddingLeft: 24, }}
+          placeholder="input to here a keywords"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+
+        <div style={{ display: 'inline-block', justifyContent: 'space-between' }}>
+          <button
+            style={{ fontSize: 20, marginRight: 8 }}
+            onClick={() => setKeyword('select * from 1')}
+          >
+            Test 'sql injection'
+          </button>
+          <button
+            style={{ fontSize: 20 }}
+            onClick={() => setKeyword('<xss id=x tabindex=1 onactivate=alert(1)></xss>')}
+          >
+            Test 'xss'
+          </button>
+        </div>
+        
+        <h3>File form</h3>
+        <input style={{ height: 80, width: '50vh', borderRadius: 20, fontSize: 24, paddingLeft: 24, }} type="file"/>
+
         <p>
           Playground for cumulus@tophat
         </p>
